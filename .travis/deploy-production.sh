eddac13d89ac7bbd458642fb0f056290d3ac0476
Savanni D'Gerinel <savanni@cloudcity.io>
2018-07-09 14:16:18 -0400

    Commit production builds to cloudcity.io:savanni

--- .travis/deploy-production.sh
+++ .travis/deploy-production.sh
@@ -8,6 +8,8 @@ cd cloudcity.io/tsomi
 cat ../../index.html | sed 's/static\/tsomi.css/\/tsomi\/static\/tsomi.css/' | sed 's/js\/bundle.js/\/tsomi\/js\/bundle.js/' > index.html
 cp -r ../../static .
 cp -r ../../dist/js .
-# git add *
-# git commit -m "deploy tsomi to production"
+git checkout -b savanni
+git add *
+git commit -m "deploy tsomi to production"
+git push --set-upstream origin savanni
 
