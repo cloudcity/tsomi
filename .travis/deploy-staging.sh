0120c21c268a256c2c7069ce7fcd85d3b1e83f69
Savanni D'Gerinel <savanni@cloudcity.io>
2018-07-31 15:00:35 -0400

    Change the source path of favicon.ico

--- .travis/deploy-staging.sh
+++ .travis/deploy-staging.sh
@@ -7,7 +7,7 @@ mkdir -p tsomi-staging
 cd tsomi-staging
 #cat ../../index.html | sed 's/static\/tsomi.css/\/tsomi\/static\/tsomi.css/' | sed 's/js\/bundle.js/\/tsomi\/js\/bundle.js/' > index.html
 cp ../index.html .
-cp ../favicon.ico .
+cp ../static/favicon.ico .
 cp -r ../static .
 cp -r ../js .
 git add *
