0b49ef0b7b37bf84272d1637e53ed8bcfffcc9ce
Autumn Grace <the@autumn.zone>
2018-11-30 10:54:24 -0500

    webpack-dev-server setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,23 @@
 const path = require('path')
 const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const FlowWebpackPlugin = require('flow-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/tsomi.js',
   devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'js'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, ""),
+    contentBase: path.join(__dirname, ''),
     compress: true,
-    port: 3000
+    port: 3000,
   },
-  plugins: [
-    new FlowWebpackPlugin()
-  ],
   module: {
     rules: [
       { test: /\.js$/, loader: 'babel-loader' },
-      { test: /\.css$/, loaders: [ 'style-loader', 'css-loader' ] }
-    ]
-  }
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
+    ],
+  },
 }
-
